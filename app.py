846d58968f846739e30a3270740ae773f1141142
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2020-03-04 04:22:29 +0000

    Update highspot app - fix python styling - add more api responses, handling for unexpected content type

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, g
-import random, sqlite3
+import random
+import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
@@ -8,17 +9,20 @@ app.config["DEBUG"] = True
 DATABASE = "database.db"
 
 
+# Function to get DB connection, returns the DB
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 # Creates the DB and table if it doesn't already exist
 def init_db():
@@ -28,6 +32,7 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+
 # Function to query database
 def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
@@ -35,54 +40,65 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
 # Check if an endpoint exist
+# Returns true, if exists, else returns false
 def get_endpoint(endpoint_id):
     id = query_db('select * from highspot_app where endpoint_id = ?',
                 [endpoint_id], one=True)
     if id is None:
         return False
-    else: 
+    else:
         return True
+
 
 # Home page
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return "<h1>Highspot API App/h1><p>Documentation: http://shorturl.at/suvF9</p>"
+
 
 # Read post data from a generated service endpoint
+# Must be content-type of application/json
 # If method is GET, retrieve the data from the database
 # Else, if POST, store the data in the database
 @app.route('/api/v1/resources/endpoint', methods=['GET', 'POST'])
-def uri_endpoint ():
+def uri_endpoint():
+    if request.content_type != 'application/json':
+        return jsonify({
+            'error': 'invalid content-type. must be application/json'
+        }), 415
     query_parameters = request.args
     endpoint_id = query_parameters.get('id', type=int)
     if get_endpoint(endpoint_id):
         if request.method == 'GET':
-            post_data = query_db('select post_data from highspot_app where endpoint_id = ?', (endpoint_id,), one=True)
-            return jsonify({'post_data': post_data })
+            post_data = query_db('select post_data from highspot_app where endpoint_id = ?',
+                                (endpoint_id,), one=True)
+            return jsonify({'post_data': post_data})
         elif request.method == 'POST':
-            data = str(request.get_data(parse_form_data=False))
-            query_db('update highspot_app set post_data = ? where endpoint_id = ?', (data, endpoint_id))
+            data = str(request.get_json())
+            query_db('update highspot_app set post_data = ? where endpoint_id = ?',
+                    (data, endpoint_id))
             db = get_db()
             db.commit()
-            return jsonify({
-                'request': data
-                }), 201
+            return jsonify({'request': data}), 200
     else:
-        return ("Endpoint not found", 404)
+        return jsonify({
+            'error': 'endpoint does not exist'
+            }), 404
 
 
 # Generates a service endpoint that can store POST data to the database
 @app.route('/api/v1/resources/endpoint/gen', methods=['GET'])
 def generate_uri():
-    value = random.randint(1000,9999)
-    uri = "/api/v1/resources/endpoint?id=" + str(value) # construct the URI
-    
+    value = random.randint(1000, 9999)
+    uri = "/api/v1/resources/endpoint?id=" + str(value)  # construct the URI
     query_db('INSERT INTO highspot_app (endpoint_id) VALUES (?)', (value,))
     db = get_db()
     db.commit()
-    return jsonify({ 'uri': uri })
+    return jsonify({'uri': uri}), 201
+
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port = 8080)
+    app.run(host='0.0.0.0', port=8080)
